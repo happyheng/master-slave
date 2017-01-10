f85c14c4a3a78e4241f628823eba3ea8633a25e2
happyheng <happyheng9522@163.com>
2017-01-10 08:02:45 +0800

    增加了Spring依赖

---
+++ master-slave-mybatis/src/main/java/com/happyheng/TestController.java
@@ -0,0 +1,18 @@
+package com.happyheng;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * test
+ * Created by happyheng on 17/1/10.
+ */
+@RestController
+public class TestController {
+
+    @RequestMapping("/test")
+    public String test() {
+        return "test";
+    }
+
+}
